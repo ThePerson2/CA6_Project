6f13a9555912cc26512f227dffe2281236150d4c
Jeff Boucher <Bouch123@umn.edu>
2016-11-06 17:26:28 +0100

    The model is really working now. Maybe half way done.

--- Simple_model.py
+++ Simple_model.py
@@ -5,72 +5,81 @@ numberGC = 10
 
 # gT = 0        # target gain. Should vary a bit depending on day of training.
 # pT = 0        # target phase shift.
-# TauPG = 15*min
+TauPG = 15*min
 w = 10*(1/second)   # rate at which the platform rotates.
 
-MF = NeuronGroup(1,'M = cos(t*w) : 1')
-GC = NeuronGroup(numberGC,'''G = cos((t*w) - x) : 1
+## make the neuron groups
+
+MF = NeuronGroup(1,'M = cos(t*w) : 1') # upon some reading... I'm not sure that the cosine was a valid choice. Will look into this more.
+GC = NeuronGroup(numberGC,model = '''G = cos((t*w) + x) : 1
 						x : 1''')
-PC = NeuronGroup(1,'P = 1 : 1')
-# S = Synapses(GC,PC,pre='P=sum(GC.G[:])')
-# Wpg = S.connect(GC,PC)
-# print(GC.G[:])
-# print(sum(GC.G[:]))
-#
-#
 for i in range(0, numberGC):
- 	GC.x[i] = (i/numberGC)*math.pi*2                # makes it so that the delays are distributed evenly
-# # help(subplot)
-# print(GC.x)
-# MF_state = StateMonitor(MF,'M',record=0)
-# GC_state = StateMonitor(GC,'G',record=range(0, numberGC))
-run(1*second)
-print(sum(GC.G[:]))
-# figure()
-# subplot(211)
-# plot(MF_state.t/ms,MF_state.M[0])
-#
-# for i in range(0, numberGC):
-# 	figure()
-# 	subplot(211)
-# 	plot(GC_state.t/ms,GC_state.G[i])
-#
-# show()
+ 	GC.x[i] = (i/numberGC)*math.pi*2        # makes it so that the delays are distributed evenly
+PC = NeuronGroup(1,model = '''P : 1
+							  V : 1''')  # V from MVN neuron
+MVN = NeuronGroup(1,model = '''M : 1
+							   P : 1
+							   V = M - P : 1
+							   eyeMovement = -V : 1''')
 
+## make the synapses
 
+Spg = Synapses(GC,PC,model='''P_post = Wpg*G_pre : 1 (summed)
+								Wpg = x_pre : 1''')
+# Spg = Synapses(GC,PC,model='''P_post = Wpg*G_pre : 1 (summed)
+# 								dWpg/dt = (x_pre*G_pre )/TauPG : 1/min''')
+Smv = Synapses(MF,MVN, model='''M_post = M_pre : 1 (summed)''')  ## I made them summed because that makes it work.
+Spv = Synapses(PC,MVN, model='''P_post = P_pre : 1 (summed)
+								V_pre = V_post : 1 (summed)''')  ## I made them summed because that makes it work.
 
-# El = -65*mV  ## leak potential
-# EK = -90*mV  ## potassium equilibrium potential
-# ENa = 50*mV  ## sodium equilibrium potential
-# tau = 20*ms	 ## time constant
-# sigma = 5.*mV
-# tau_channel = 2*ms
-# tau_m = 0.2*ms
-# g = 1000 # g leak is 1, this is relative to that.
-# I = 2000*mV
-#
-# equations =  '''dv/dt = (El-v - g*m**3*h*(v-ENa) - g*n**4*(v-EK) + I)/tau : volt #
-#
-# 				dm/dt = (1./(1+exp((-v-40*mV)/sigma)) - m)/tau_m : 1
-# 				dh/dt = (1./(1+exp( (v+40*mV)/sigma)) - h)/tau_channel : 1
-# 				dn/dt = (1./(1+exp((-v-40*mV)/sigma)) - n)/tau_channel : 1'''
-#
-# neuron = NeuronGroup(1,equations, method='exponential_euler')
-# voltage = StateMonitor(neuron,'v',record=0)
-# m = StateMonitor(neuron,'m',record=0)
-# n = StateMonitor(neuron,'n',record=0)
-# h = StateMonitor(neuron,'h',record=0)
-# neuron.v = El
-# neuron.m = 0
-# neuron.h = 1
-# neuron.n = 0
-#
-# run(50*ms)
-#
-# figure()
-# subplot(211)
-# plot(voltage.t/ms,voltage.v[0]/mV)
-# subplot(212)
-# plot(m.t/ms,(m.m[0])**3*h.h[0])
-# plot(n.t/ms,(n.n[0])**4)
-# show()
+## connect the synapses
+
+Spg.connect()
+Smv.connect()
+Spv.connect()
+
+## create the state monitors
+
+MF_state = StateMonitor(MF,'M',record=0)
+# GC_state = StateMonitor(GC,'G',record=True)
+PC_state = StateMonitor(PC,'P',record=0)
+MVN_state = StateMonitor(MVN,'eyeMovement',record=0)
+
+## run the model
+
+run(0.1*second)
+print(MVN.M[:])
+print(Smv.M_post[:])
+print(Smv.M_pre[:])
+print()
+run(0.1*second)
+print(MVN.M[:])
+print(Smv.M_post[:])
+print(Smv.M_pre[:])
+run(0.9*second)
+
+## plot results
+
+if 'MF_state' in locals():
+	figure()
+	subplot(211)
+	plot(MF_state.t/ms,MF_state.M[0])
+
+if 'GC_state' in locals():
+	for i in range(0, numberGC):
+		figure()
+		subplot(211)
+		plot(GC_state.t/ms,GC_state.G[i])
+if 'PC_state' in locals():
+	figure()
+	subplot(211)
+	plot(PC_state.t/ms,PC_state.P[0])
+
+if 'MVN_state' in locals():
+	figure()
+	subplot(211)
+	plot(MVN_state.t/ms,MVN_state.eyeMovement[0])
+	xlabel('time')
+	ylabel('Eye movement')
+
+show()
